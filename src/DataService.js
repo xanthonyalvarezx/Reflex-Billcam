35a3baf6433e61fb1ebae37118adbdd04372eff3
AntMan <xanthonyalvarezx@gmail.com>
2020-11-26 20:49:20 -0500

    commiting before pull

--- src/DataService.js
+++ src/DataService.js
@@ -7,7 +7,7 @@ const DataService = () => {
   useEffect(() => {
     axios
       .get("https://www.googleapis.com/youtube/v3/videos&api_key=AIzaSyDjokEZzqF2e8a0n4Jcla0wRyUYqCvMFyU")
-      .then((result) => getData(JSON.stringify(result.data)));
+      .then((result) => getData(result.data));
   }, []);
 
   return (
--- src/key.js
+++
@@ -1,9 +0,0 @@
-import Videos from "./Components/videos";
-
-export const keys = () => {
-  return (
-    <>
-      <Videos key="AIzaSyDjokEZzqF2e8a0n4Jcla0wRyUYqCvMFyU" />
-    </>
-  );
-};
